55318160a8f93c2e313f59cae028a1e0465e22d1
villavargas <villa_vargas@hotmail.com>
2016-04-24 12:50:15 -0500

    commit

--- PeticionServidor/ViewController.swift
+++ PeticionServidor/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate  {
 
+    
+    @IBOutlet weak var ISBN: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ISBN.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +27,40 @@ class ViewController: UIViewController {
     }
 
 
+   
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    
+    
+    @IBAction func backgroundTap(sender: UIControl) {
+          ISBN.resignFirstResponder()
+    }
+  
+    
+    
+    @IBAction func textFieldDidEndEditing(textField: UITextField) {
+        print("Inicia peticion al servidor...")
+        sincrono()
+        print(ISBN.text)
+    }
+    
+    func sincrono(){
+        
+        if let isbn = ISBN.text {
+        let baseURL = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn)")
+        
+            
+        let datos:NSData? = NSData(contentsOfURL: baseURL!)
+        let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
+        print(texto)
+        self.textView!.text = String(texto)
+        }else{
+            print("Error")
+        }
+
+    }
+    
 }
 
