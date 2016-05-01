c5308c60c76df988172c67307ebfbefd06085d2c
villavargas <villa_vargas@hotmail.com>
2016-05-01 17:33:04 -0500

    commit

--- PeticionServidor/ViewController.swift
+++ PeticionServidor/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController,UITextFieldDelegate  {
     @IBOutlet weak var textView: UITextView!
     
     
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         ISBN.delegate = self
@@ -55,12 +57,89 @@ class ViewController: UIViewController,UITextFieldDelegate  {
         let datos:NSData? = NSData(contentsOfURL: baseURL!)
         let texto = NSString(data:datos!, encoding: NSUTF8StringEncoding)
         print(texto)
-        self.textView!.text = String(texto)
+            var titulo = ""
+            var autores = ""
+        
+            do {
+                
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableContainers)
+                
+                let dico1 = json as! NSDictionary
+                
+                if let dico2 = dico1["ISBN:\(isbn)"] as? NSDictionary {
+                    
+                    let dico3 = dico2["authors"] as? [NSDictionary]
+                    
+                    titulo = "\(dico2["title"] as! NSString as String)"
+                    
+                    // Existe portada?
+                    
+                    if let dico4 = dico2["cover"] as? NSDictionary {
+                        
+                        let url = NSURL(string: dico4["medium"] as! NSString as String)
+                        cargar_imagen(url!)
+                        
+                    }
+                    
+                    if let dicAutores = dico3 {
+                        
+                        for autor in dicAutores {
+                            
+                            autores += "\(autor["name"] as! NSString as String) "
+                            
+                        }
+                        
+                    }
+                    
+                } else {
+                    
+                    titulo = "ISBN no válido"
+                    
+                }
+                
+            } catch _ {
+                
+                print("Error")
+                
+            }
+            
+
+            
+         self.textView!.text = String("Titulo: \(titulo) \n\n\n" +
+                                      "Autor: \(autores)"   )
         }else{
             print("Error")
         }
 
     }
+    
+    
+    
+    func cargar_imagen(urlString:NSURL) {
+        let imgURL = urlString
+        let request: NSURLRequest = NSURLRequest(URL: imgURL)
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request){
+            (data, response, error) -> Void in
+            
+            if (error == nil && data != nil)
+            {
+                func display_image()
+                {
+                    self.image.image = UIImage(data: data!)
+                    self.image.hidden = false
+                    
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), display_image)
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
     
 }
 
